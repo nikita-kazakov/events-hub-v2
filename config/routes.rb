5999600df7c7c9677482c035cf93dd61214fab7b
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-07-03 20:03:44 -0400

    add session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resource :session
+
 
   resources :users
+
+
   get 'signup' => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
