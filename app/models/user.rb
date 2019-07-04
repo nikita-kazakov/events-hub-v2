a3d64d41b81d5361e19cf8fc499d8172ba984ef9
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-07-04 19:21:18 -0400

    add logging in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :username, presence: true,
             format: /\A[A-Z0-9]+\z/i,
             uniqueness: { case_sensitive: false }
-  #Error test again
+
 
 end
